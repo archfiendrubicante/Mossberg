3666c521a7a309801e35fb126ca5c589c51220f1
Nuwa Nightstone <archfiendrubicante@gmail.com>
2021-11-03 08:22:54 -0400

    rename all

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const MOSSBERG = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ MOSSBERG.abilities = {
+  "str": "MOSSBERG.AbilityStr",
+  "dex": "MOSSBERG.AbilityDex",
+  "con": "MOSSBERG.AbilityCon",
+  "int": "MOSSBERG.AbilityInt",
+  "wis": "MOSSBERG.AbilityWis",
+  "cha": "MOSSBERG.AbilityCha"
+};
+
+MOSSBERG.abilityAbbreviations = {
+  "str": "MOSSBERG.AbilityStrAbbr",
+  "dex": "MOSSBERG.AbilityDexAbbr",
+  "con": "MOSSBERG.AbilityConAbbr",
+  "int": "MOSSBERG.AbilityIntAbbr",
+  "wis": "MOSSBERG.AbilityWisAbbr",
+  "cha": "MOSSBERG.AbilityChaAbbr"
+};
